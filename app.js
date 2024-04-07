9c41af371b071bdf1cade15ae83c4e24a37c719f
toyah1427 <toyahdegroat1427@gmail.com>
2024-04-07 09:26:54 +0100

    images added

--- app.js
+++ app.js
@@ -2,7 +2,41 @@ const thumbContainer = document.getElementById("thumb-container");
 const displayImage = document.getElementById("display");
 
 //add images
-const images = []
+const images = [
+    {
+        url: "https://images.unsplash.com/photo-1495012379376-194a416fcc5f?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "White jellyfish swimming",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1543007168-5fa9b3c5f5fb?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Pink and orange jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1540968221243-29f5d70540bf?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Yellow and white jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1590013330451-3946e83e0392?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Brown jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1611304170711-0a02d1b23329?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGplbGx5ZmlzaHxlbnwwfHwwfHx8Mg%3D%3D", alt: "Bright pink and white jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1634481298300-00a65c50a3da?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Jellyfish close-up",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1547103106-9a0e718bb2d2?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Four orange jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1613374933062-7aa9f54036ce?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Blue and pink jellyfish illustration",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1613031260991-32a5b283e53f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGplbGx5ZmlzaHxlbnwwfHwwfHx8Mg%3D%3D", alt: "Pink and red jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1549741501-4211de5d3757?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fGplbGx5ZmlzaHxlbnwwfHwwfHx8Mg%3D%3D", alt: "White and yellow jellyfish",
+    },
+    {
+        url: "https://images.unsplash.com/photo-1626345545815-650a098a4c76?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "Blue jellyfish black background",
+    },
+];
 
 function createThumbnails() {
     images.forEach(function (image) {
